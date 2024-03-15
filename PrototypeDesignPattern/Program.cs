32ba6643a13441ce8cd3759a989e8c3a8336d009
naimolcer <naimolcer@gmail.com>
2024-03-15 11:09:58 +0300

    Add project files.

---
+++ PrototypeDesignPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrototypeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ins1 = new Person();
+
+            ins1.SetName("Naim");
+            ins1.SetSurame("Ölçer");
+            ins1.SetAge(40);
+
+            Person ins2 = ins1.Clone();
+
+            Console.WriteLine("Instance 1");
+            ins1.Print();
+
+            Console.WriteLine("Instance 2");
+            ins2.Print();
+
+            ins1.SetAge(42);
+            ins1.SetName("Naim1");
+            ins1.SetSurame("Ölçer2");
+
+            Console.WriteLine("Instance 1 After Change");
+            ins1.Print();
+
+            Console.WriteLine("Instance 2 After Change");
+            ins2.Print();
+
+        }
+    }
+}
